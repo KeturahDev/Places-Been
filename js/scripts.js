9c85b4d1f76b13d75e8f55bcc25f815641b962f2
Keturah Howard and Sommer Howe <sommer@>
2020-01-27 14:20:14 -0800

    skip 5th spec and complete 6th and final spac.. displau object properties as list items

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+// Buisness Logic ----------------------------------------------------------------------
 //places buisness logic ---------------------
 function Places () {
   this.places = [];
@@ -13,8 +14,8 @@ function Place (country, city, date, landmark, note) {
   this.country = country,
   this.city = city,
   this.date = date,
-  this.landmark = landmark,
-  this.note = note
+  this.landmark = landmark
+  // this.note = note
 }
 
 var alaska = new Place('USA', 'Juno', 7 , 'Glacier'); 
@@ -26,3 +27,26 @@ places.addPlace(alaska);
 places.addPlace(vegas); 
 
 console.log('Places', places);
+
+// var listIt = function(place) {
+//   var listedProperties = place.forEach(function() {
+
+//   })
+
+// }
+
+// UserInterface Logic ---------------------------------------------------------------------
+$(document).ready(function(){
+  $("#vegas").click(function(){
+    $("ul#vegas").append("<li>" + vegas.country + '</li>')
+    $("ul#vegas").append("<li>" + vegas.city + '</li>')
+    $("ul#vegas").append("<li>" + vegas.date + '</li>')
+    $("ul#vegas").append("<li>" + vegas.landmark + '</li>')
+  })
+  $("#alaska").click(function(){
+    $("ul#alaska").append("<li>" + alaska.country + '</li>')
+    $("ul#alaska").append("<li>" + alaska.city + '</li>')
+    $("ul#alaska").append("<li>" + alaska.date + '</li>')
+    $("ul#alaska").append("<li>" + alaska.landmark + '</li>')
+  })
+})
