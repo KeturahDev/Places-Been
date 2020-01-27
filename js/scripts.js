0c80b23dee55d23b7c1084453852b3c4206af1b6
Keturah Howard and Sommer Howe <keturah@>
2020-01-27 12:00:43 -0800

    add bootstrap and jquery, and add constructors to scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+//places buisness logic ---------------------
+function Places () {
+  this.place = []
+  this.id = 0 
+}
+
+Places.prototype.addPlace(place) {
+  this.pla
+};
+
+// place buisness logic ---------------------
+function Place (country, city, date, landmark, note) {
+  this.country = country,
+  this.city = city,
+  this.landmark = landmark,
+  this.note = note
+}
